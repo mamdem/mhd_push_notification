d68d7103eb43e390ee95c866bd2601b1ccda7dcb
mamdem <houma.mamour@gmail.com>
2025-02-14 01:27:44 +0000

    first commit

--- test/mhd_push_notification_test.dart
+++ test/mhd_push_notification_test.dart
@@ -4,10 +4,10 @@ void main() async {
   final Map<String, dynamic> serviceAccount = {
     "type": "service_account",
     "project_id": "sengreen",
-    "private_key_id": "742e7d90becfacf12c4a6914d4989c5c5698e768",
+    "private_key_id": "secret_key_id",
     "private_key": "-----BEGIN PRIVATE KEY-----\n...",
     "client_email": "firebase-adminsdk-ymiqg@sengreen.iam.gserviceaccount.com",
-    "client_id": "110613643083102533029",
+    "client_id": "client_id",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
